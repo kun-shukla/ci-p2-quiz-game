4aec48c7a22f34801b80b693f0014b2ce198ab13
kunalshukla <kun.shukla@gmail.com>
2024-01-11 13:07:05 +0000

    Create QA's array, and place correct answer in position [0] of each sub-array

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
 console.log("Hello World");
+
+let wildlifeQuiz = [
+  [
+    "What is the largest big cat in the world?",
+    ["Tiger", "Lion", "Cheetah", "Jaguar"],
+  ],
+  ["Which mammal can fly?", ["Bat", "Penguin", "Kangaroo", "Elephant"]],
+  [
+    "What is the world's largest land mammal?",
+    ["Elephant", "Giraffe", , "Rhino", "Hippopotamus"],
+  ],
+
+  [
+    "Which bird is known for its ability to imitate human speech?",
+    ["Parrot", "Owl", "Eagle", "Penguin"],
+  ],
+
+  [
+    "What is the largest species of bear?",
+    ["Polar Bear", "Black Bear", "Grizzly Bear", "Panda Bear"],
+  ],
+
+  [
+    "What is the world's fastest land animal?",
+    ["Cheetah", "Gazelle", "Lion", "Zebra"],
+  ],
+
+  [
+    "Which animal is known for its humps and is native to deserts?",
+    ["Camel", "Horse", "Elephant", "Rhino"],
+  ],
+
+  [
+    "What is the largest species of shark?",
+    ["Whale Shark", "Great White Shark", "Hammerhead Shark", "Tiger Shark"],
+  ],
+
+  [
+    "Which reptile is known for changing the color of its skin?",
+    ["Chameleon", "Iguana", "Turtle", "Crocodile"],
+  ],
+
+  [
+    "What is the only marsupial found in North America?",
+    ["Opossum", "Kangaroo", "Wallaby", "Koala"],
+  ],
+];
+
+console.log(wildlifeQuiz[1][1][3]);
